2ebbbd264aaa5c4735219b7cc8ee5b255e0475bf
Илья Ковальчук <samsung.fly64@gmail.com>
2018-12-03 23:48:36 +0300

    Update Ril

--- lineage_alps.mk
+++ lineage_alps.mk
@@ -165,7 +165,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 	dalvik.vm.image-dex2oat-Xmx=64m \
 	ro.dalvik.vm.native.bridge=0 \
 	ro.hardware=sprout \
-	ro.telephony.ril_class=MediaTekRIL
+	ro.telephony.ril_class=SproutRIL
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     pm.dexopt.first-boot=verify-at-runtime \
