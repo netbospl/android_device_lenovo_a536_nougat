139da83bc3a131af369cc39d90b8cf3f6044684d
Piotr Matela <piotrekmat@protonmail.com>
2021-02-15 11:54:15 +0000

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-cd ../../..
-cd system/core
+##run from lineage root
+CWD=$PWD
+cd $CWD/system/core
 patch -p1 < ../../device/alps/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 patch -p1 < ../../device/alps/patches/0020-healthd_batteryVoltage.patch
-cd ../..
-cd bionic
+cd $CWD/bionic
 patch -p1 < ../device/alps/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
-cd ../../..
-cd frameworks/av
+cd $CWD/frameworks/av
 patch -p1 < ../../device/alps/patches/0006-fix-access-wvm-to-ReadOptions.patch
 patch -p1 < ../../device/alps/patches/0007-Disable-usage-of-get_capture_position.patch
 patch -p1 < ../../device/alps/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders.patch
 patch -p1 < ../../device/alps/patches/0009-add-mtk-color-format-support.patch
-cd ../..
-cd system/netd
+cd $CWD/system/netd
 patch -p1 < ../../device/alps/patches/netd.patch
-cd ../..
-
+cd $CWD/
